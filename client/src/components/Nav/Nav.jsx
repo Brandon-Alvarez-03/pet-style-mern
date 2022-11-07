cc69e6e9f57ffce44780b524f07fa0c0b9c27b82
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-07 10:07:31 -0500

    made css file for nav and added them into a folder for only nav

---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className='nav'>
+      <h2>Placeholder</h2>
+      <h2>Blah</h2>
+      <h2>Test</h2>
+    </div>
+  )
+}
+
+export default Nav
