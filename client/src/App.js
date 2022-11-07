17dab7e0e1c976c172cc2334b7195a7e4538ea29
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-07 16:06:46 -0500

    imported footer into the file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Nav from "./components/Nav/Nav.jsx";
 import Main from "./components/Main.jsx";
-import Footer from "./components/Footer.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import "./App.css";
 
 function App() {
