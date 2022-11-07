9a12fc7a6477121e8ca3dde7f2a6a7a1898b4833
“Brandon <balvarez325@gmail.com>
2022-11-06 23:19:41 -0500

    comment out unused imports for npm build

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
-import Home from "../screens/Home.jsx";
+// import {Routes, Route} from "react-router-dom";
+// import Home from "../screens/Home.jsx";
 import Products from "../screens/Products.jsx";
 
 function Main() {
