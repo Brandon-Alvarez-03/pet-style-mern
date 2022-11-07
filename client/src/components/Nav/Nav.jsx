d759b0fa5836fc74b1dd274a04bc486e8a29878d
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-07 10:47:29 -0500

    rendered logo on navbar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Nav.css"
+import logo from "../../images/logos/PetLyfe-logo.png"
 
 function Nav() {
   return (
     <div className='nav'>
       <h2>Placeholder</h2>
-      <h2>Blah</h2>
+      <img className='nav-logo' alt='PetLyfe-logo' src={ logo } />
       <h2>Login</h2>
     </div>
   )
