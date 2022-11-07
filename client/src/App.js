247cd697ede3d034325d33a8de7f617f1be510e7
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-07 10:06:33 -0500

    fixed import paths, nav files in their own folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import Nav from "./components/Nav.jsx";
+import Nav from "./components/Nav/Nav.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 import "./App.css";
