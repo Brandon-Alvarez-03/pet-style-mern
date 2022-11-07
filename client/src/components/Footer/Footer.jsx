79a3bc3df51fe4e2772ffd7ef3d254d4b1296953
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-07 16:23:43 -0500

    added scroll to top logic, applied it to paws logo onclick

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-import logo from "../../images/logos/PetLyfe-logo.png"
+import React, { useState } from "react";
+import Translogo from "../../images/logos/PetLyfe-logo-removebg.png"
 import "./Footer.css"
 import pawsLogo from "../../images/logos/PetLyfe-paws.png"
 
 function Footer() {
+
+
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    } 
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
+  
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+  
+  window.addEventListener('scroll', toggleVisible);
+
+
+
   return <div className="footer">
-    <img className="footer-logo" alt='PetLyfe-logo' src={logo} />
+    <img className="footer-logo" alt='PetLyfe-logo' src={Translogo} />
     <div className="footer-about">
       <h2>About Us!</h2>
     </div>
-    <img className="footer-paws" alt='PetLyfe-Paws' src={ pawsLogo } />
+    <img className="footer-paws" alt='PetLyfe-Paws' src={pawsLogo} onClick={scrollToTop} />
   </div>;
 }
 
