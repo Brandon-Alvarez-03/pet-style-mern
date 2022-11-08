748e5e8d52c64af670b69d1dd4c810287acdafce
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-08 11:28:17 -0500

    remove node_modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> c8f6897712870b6ab3e922c55c243cf6bd145d1b
 import Nav from "./components/Nav/Nav.jsx";
 import Main from "./components/Main/Main.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> c8f6897712870b6ab3e922c55c243cf6bd145d1b
 import "./App.css";
-// import Products from "./screens/Products.jsx";
 
 function App() {
   return (
