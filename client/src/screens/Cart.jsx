dd3c4dd8624f57c463c7f5478072fc262ab627b1
CapriceLoren <capriceloren@gmail.com>
2022-11-08 12:16:12 -0500

    edit cart

--- client/src/screens/Cart.jsx
+++ client/src/screens/Cart.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom"
+import "../Cart.css"
+import product from "./Products.jsx"
 
 function Cart() {
-  return <div>Shopping Cart</div>;
+  return (
+
+    // if no items, link to /emptycart
+<div className="bg">
+    <div className="container">
+      <div className="items">
+          <p>Shopping Cart</p>
+          {cart.map((product, index) => {
+            return (
+        <div key={index} className="product">
+              <img
+                className="product-image"
+                src={product.img_thumb}
+                alt={product.name}
+              />
+              <p>{product.product_name}</p>
+              <p>Price: {product.retail_price}</p>
+              <p>Rating: {product.rating}</p>
+              </div>)
+            })}
+      </div>
+
+      <div className="subTotal">
+        <p>Subtotal</p>
+        <Link to="/checkout"><button>Proceed to Checkout</button></Link>
+      </div>
+      </div>
+      </div>
+  );
 }
 
 export default Cart;
