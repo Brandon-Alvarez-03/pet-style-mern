b562bd8dd1e735352fbf35a6a5e4715b4eab7c71
“Brandon <balvarez325@gmail.com>
2022-11-07 19:44:10 -0500

    push for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
+// import {Routes, Route} from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import Main from "./components/Main.jsx";
+// import Home from "./screens/Home.jsx";
+// import Products from "./screens/Products.jsx";
 import Footer from "./components/Footer.jsx";
 import "./App.css";
 
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-// import {Routes, Route} from "react-router-dom";
-// import Home from "../screens/Home.jsx";
+import {Routes, Route} from "react-router-dom";
+import Home from "../screens/Home.jsx";
 import Products from "../screens/Products.jsx";
+import Cart from "../screens/Cart.jsx";
+import Checkout from "../screens/Checkout.jsx";
 
 function Main() {
   return (
     <div className="main">
-      Screens go in here!!!
-      {/* <Home /> */}
-      <Products />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </div>
   );
 }
