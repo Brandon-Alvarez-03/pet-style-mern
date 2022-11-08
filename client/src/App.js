a6c4f32b1d139d93a35781aec39f6847768e303c
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-08 18:25:14 -0500

    switched comps aronid in app function for testing

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Main from "./components/Main/Main.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import "./App.css";
 import SignIn from "./screens/Sign-In/SignIn.jsx";
+import SignUp from "./screens/Sign-Up/SignUp.jsx";
+import Products from "./screens/Products.jsx";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <SignIn />
+      <Main />
       <Footer />
     </div>
   );
