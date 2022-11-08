100991eafecc677f9afe5a46a1ca6fdcb46afa96
“Brandon <balvarez325@gmail.com>
2022-11-07 19:49:55 -0500

    retry for test

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,29 +1,19 @@
 import React from "react";
-<<<<<<< HEAD
 import {Routes, Route} from "react-router-dom";
-import Home from "../screens/Home.jsx";
-=======
-// import {Routes, Route} from "react-router-dom";
-import Home from "../screens/Home/Home.jsx";
->>>>>>> 3fdc6773a940080366301e7aa8287e227feb811c
 import Products from "../screens/Products.jsx";
 import Cart from "../screens/Cart.jsx";
 import Checkout from "../screens/Checkout.jsx";
+import Home from "../screens/Home/Home.jsx";
 
 function Main() {
   return (
     <div className="main">
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
-=======
-      <Home />
-      {/* <Products /> */}
->>>>>>> 3fdc6773a940080366301e7aa8287e227feb811c
     </div>
   );
 }
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import mainPic from '../../images/catsAndDogs/cat-dog-3.jpg';
-import catPic from '../../images/cats/cat-1.jpg';
-import dogPic from '../../images/dogs/dog-6.jpg';
-import allPic from '../../images/catsAndDogs/cat-dog-2.jpg';
-import './Home.css';
+import React from "react";
+import mainPic from "../../images/catsAndDogs/cat-dog-3.jpg";
+import catPic from "../../images/cats/cat-1.jpg";
+import dogPic from "../../images/dogs/dog-6.jpg";
+import allPic from "../../images/catsAndDogs/cat-dog-2.jpg";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className='homeScreen'>
-
-      <div className='homeImage'>
-        <img className='landingImage' alt='cats and dogs with pink background' src={mainPic}/>
-        <h3 className='CTA'>Spoil your fur baby</h3>
-        <button className='CTAButton'>Shop Now</button>
+    <div className="homeScreen">
+      <div className="homeImage">
+        <img
+          className="landingImage"
+          alt="cats and dogs with pink background"
+          src={mainPic}
+        />
+        <h3 className="CTA">Spoil your fur baby</h3>
+        <button className="CTAButton">Shop Now</button>
       </div>
 
       <h3>Shop by Pet</h3>
-      
-      <div className='choosePet'>
-        <div className='chooseCat'>
-          <img className='chooseCatImg' alt='cats' src={catPic}/>
-          <button className='shopCats'>Shop Cats</button>
-        </div>
-        <div className='chooseDog'>
-          <img className='chooseDogImg' alt='cats' src={dogPic}/>
-          <button className='shopDogs'>Shop Dogs</button>
-        </div>
-        <div className='chooseAll'>
-          <img className='chooseAllImg' alt='cats' src={allPic}/>
-          <button className='shopAll'>Shop All</button>
-        </div>
 
+      <div className="choosePet">
+        <div className="chooseCat">
+          <img className="chooseCatImg" alt="cats" src={catPic} />
+          <button className="shopCats">Shop Cats</button>
+        </div>
+        <div className="chooseDog">
+          <img className="chooseDogImg" alt="cats" src={dogPic} />
+          <button className="shopDogs">Shop Dogs</button>
+        </div>
+        <div className="chooseAll">
+          <img className="chooseAllImg" alt="cats" src={allPic} />
+          <button className="shopAll">Shop All</button>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
