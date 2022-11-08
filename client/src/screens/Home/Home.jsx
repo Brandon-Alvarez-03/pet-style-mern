5c00cc51b59f490160b9bfbb3f8ed41476408304
erinbaright <erin.baright29@gmail.com>
2022-11-08 17:01:54 -0500

    fixed alignment in Products.css

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -8,15 +8,18 @@ import catClothes from '../../images/categories/cat-clothes.jpg';
 import dogToys from '../../images/categories/dog-toys.jpg';
 import dogClothes from '../../images/categories/dog-clothes.jpg';
 import './Home.css';
+import {Link} from "react-router-dom";
 
 function Home() {
   return (
     <div className='homeScreen'>
 
       <div className='homeImage'>
-        <img className='landingImage' alt='cats and dogs with pink background' src={mainPic}/>
+        <img className='landingImage' alt='cats and dogs with pink background' src={mainPic} />
         <h2 className='CTA'>Spoil your fur baby.</h2>
-        <button className='CTAButton'>Shop Now</button>
+        <Link to="/products">
+          <button className='CTAButton'>Shop Now</button>
+        </Link>
       </div>
 
       <div className='description'>
@@ -26,37 +29,57 @@ function Home() {
       <h2>Choose Your Pet</h2>
       <div className='choosePet'>
         <div className='chooseCat'>
-          <img className='chooseCatImg' alt='sleeping cat' src={catPic}/>
-          <button className='shopCats'>Shop Cats</button>
+          <Link to="/products/cats">
+            <img className='chooseCatImg' alt='sleeping cat' src={catPic} />
+          </Link>    
+          <Link to="/products/cats">
+            <button className='shopCats'>Shop Cats</button>
+          </Link>
         </div>
         <div className='chooseDog'>
+          <Link to="/products/dogs">
           <img className='chooseDogImg'
             alt='dog wearing sunglasses' src={dogPic}
-          />
+            />
+            </Link> 
+            <Link to="/products/dogs">
           <button className='shopDogs'>Shop Dogs</button>
+          </Link> 
         </div>
         <div className='chooseAll'>
-          <img className='chooseAllImg' alt='cat and dog' src={allPic}/>
+          <Link to="/products">
+            <img className='chooseAllImg' alt='cat and dog' src={allPic} />
+          </Link>
+          <Link to="/products">
           <button className='shopAll'>Shop All</button>
+          </Link>
         </div>
       </div>
 
       <h2 className='popular-categories'>Popular Categories</h2>
       <div className='categoriesContainer'>
         <div className='cat-toys'>
-          <img className='cat-toy-pic' alt='cat toy' src={catToys} />
+        <Link to="/products/cats/toys">
+            <img className='cat-toy-pic' alt='cat toy' src={catToys} />
+            </Link>
           <h3>Cat Toys</h3>
         </div>
         <div className='cat-clothes'>
-          <img className='cat-clothes-pic' alt='cat clothes' src={catClothes} />
+        <Link to="/products/cats/clothes">
+            <img className='cat-clothes-pic' alt='cat clothes' src={catClothes} />
+            </Link>
           <h3>Cat Clothes</h3>
         </div>
         <div className='dog-toys'>
-          <img className='dog-toy-pic' alt='dog toy' src={dogToys} />
+        <Link to="/products/dogs/toys">
+            <img className='dog-toy-pic' alt='dog toy' src={dogToys} />
+            </Link>
           <h3>Dog Toys</h3>
         </div>
         <div className='dog-clothes'>
-          <img className='dog-clothes-pic' alt='dog clothes' src={dogClothes} />
+        <Link to="/products/dogs/clothes">
+            <img className='dog-clothes-pic' alt='dog clothes' src={dogClothes} />
+            </Link>
           <h3>Dog Clothes</h3>
         </div>
       </div>
