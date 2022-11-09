192cde36bf35cd0331e01380e1ba282e88f7582d
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-09 14:41:53 -0500

    moved signup button and account icon into cart nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,40 +3,35 @@ import "./Nav.css";
 import logo from "../../images/logos/PetLyfe-logo-removebg.png";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
-import PersonIcon from '@mui/icons-material/Person';
-
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
 
 function Nav() {
-
- 
-
-    return (
-      <>
-        <div className="nav">
-          <MenuIcon
-            className="ham-menu-icon"
-          />
-          <Link to="/">
-            <img className="nav-logo" alt="PetLyfe-logo" src={logo} />
-          </Link>
-          <div className="account-buttons">
-          <Link to="/signup">
-           <button className="signup">Sign Up</button>
-            </Link>
-            <Link to="/login">
-              {/* <button className="login">Login</button> */}
-              <PersonIcon />
-              </Link>
-            </div>
-          <div className="cart-container">
-            <Link to="/cart">
-              <ShoppingCartIcon className="shopping-cart" />
-            </Link>
-          </div>
+  return (
+    <>
+      <div className="nav">
+        <MenuIcon className="ham-menu-icon" />
+        <Link to="/">
+          <img className="nav-logo" alt="PetLyfe-logo" src={logo} />
+        </Link>
+        <div className="account-buttons">
         </div>
-      </>
-    );
-  }
+        <div className="cart-container">
+          <Link to="/signup">
+            <button className="signup">Sign Up</button>
+          </Link>
+          <Link to="/login">
+            <div id="person-icon">
+              <PersonIcon className="person-icon" />
+            </div>
+          </Link>
+          <Link to="/cart">
+            <ShoppingCartIcon className="shopping-cart" />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Nav;
