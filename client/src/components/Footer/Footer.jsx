0ab9811803e30094c932a941524cb834bb33099e
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-08 19:28:38 -0500

    added github links for everyone, changed login button to icon

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,47 +1,82 @@
 import React, { useState } from "react";
-import Translogo from "../../images/logos/PetLyfe-logo-removebg.png"
-import "./Footer.css"
-import pawsLogo from "../../images/logos/PetLyfe-paws.png"
-import CopyrightIcon from '@mui/icons-material/Copyright';
+import Translogo from "../../images/logos/PetLyfe-logo-removebg.png";
+import "./Footer.css";
+import pawsLogo from "../../images/logos/PetLyfe-paws.png";
+import GitLogo from "../../images/logos/GitHub32px.png";
+
 
 function Footer() {
+  const [visible, setVisible] = useState(false);
 
+  const handleClickSandy = () => {
+    window.open("https://github.com/limbocaba", "_blank")  
+  }
+  const handleClickBrandon = () => {
+    window.open("https://github.com/Brandon-Alvarez-03", "_blank")  
+  }
+  const handleClickGary = () => {
+    window.open("https://github.com/garybones86", "_blank")  
+  }
+  const handleClickErin = () => {
+    window.open("https://github.com/erinbaright", "_blank")  
+  }
+  const handleClickCap = () => {
+    window.open("https://github.com/CapriceLoren", "_blank")  
+  }
 
-  const [visible, setVisible] = useState(false)
-  
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 300){
-      setVisible(true)
-    } 
-    else if (scrolled <= 300){
-      setVisible(false)
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
     }
   };
-  
-  const scrollToTop = () =>{
+
+  const scrollToTop = () => {
     window.scrollTo({
-      top: 0, 
-      behavior: 'smooth'
+      top: 0,
+      behavior: "smooth",
       /* you can also use 'auto' behaviour
          in place of 'smooth' */
     });
   };
-  
-  window.addEventListener('scroll', toggleVisible);
 
+  window.addEventListener("scroll", toggleVisible);
 
-
-  return <div className="footer">
-    <img className="footer-logo" alt='PetLyfe-logo' src={Translogo} />
-    <div className="footer-about-copy">
-      <h2>About Us!</h2>
-      <div className="copyright-icon">
-        <CopyrightIcon /> <h6>PetLyfe 2022</h6>
+  return (
+    <div className="footer">
+      <img className="footer-logo" alt="PetLyfe-logo" src={Translogo} />
+      <div className="footer-middle">
+        <div className="sandy-git">
+        <h3>Sandy C.</h3>
+          <img alt="Sandys GitHub" src={GitLogo} onClick={ handleClickSandy } />
         </div>
+        <div className="brandon-git">
+          <h3>Brandon A.</h3>
+          <img alt="Brandons GitHub" src={GitLogo} onClick={ handleClickBrandon } />
+        </div>
+        <div className="cap-git">
+          <h3>Caprice G.</h3>
+          <img alt="Caprices GitHub" src={GitLogo} onClick={ handleClickCap } />
+        </div>
+        <div className="erin-git">
+          <h3>Erin B.</h3>
+          <img alt="Erin GitHub" src={GitLogo} onClick={ handleClickErin } />
+        </div>
+        <div className="gary-git">
+          <h3>Gary B.</h3>
+          <img alt="Garys GitHub" src={GitLogo} onClick={ handleClickGary} />
+        </div>
+      </div>
+      <img
+        className="footer-paws"
+        alt="PetLyfe-Paws"
+        src={pawsLogo}
+        onClick={scrollToTop}
+      />
     </div>
-    <img className="footer-paws" alt='PetLyfe-Paws' src={pawsLogo} onClick={scrollToTop} />
-  </div>;
+  );
 }
 
 export default Footer;
