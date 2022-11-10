0940b6549be73f48f255bb6b954dcfcda71ee506
“Brandon <balvarez325@gmail.com>
2022-11-10 09:30:03 -0500

    update readme

--- client/src/screens/Cart.jsx
+++ client/src/screens/Cart.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, isValidElement} from "react";
+import React, {useState, useEffect} from "react";
 import {useNavigate, Link} from "react-router-dom";
 import "../Cart.css";
 
